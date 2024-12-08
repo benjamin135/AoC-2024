19cc10ac7d846a170ab1ff01a9091fcf9f3112df
Benjamin Katz <benckatz@gmail.com>
2024-12-08 01:57:18 -0500

    Solved 8a

--- day_8/day_8a.py
+++ day_8/day_8a.py
@@ -4,20 +4,18 @@ Tested using:
 
 
 Plan of attack:
-    1) Identify list of all frequencies.
-    2) For each frequency, identify all instances.
-    3) For each frequency, identify all pairs of instances.
-    4) For each pair, identify the indexes of the two antinodes.
-    5) For each antinode, confirm that it doesn't lie out of bounds (disregard it otherwise).
-    6) Count all antinodes which have not been disregarded.
+    1) For each frequency, identify all instances.
+    2) For each frequency, identify all pairs of instances.
+    3) For each pair, identify the indexes of the two antinodes.
+    4) Count the number of antinodes which do not lie out of bounds
 
 
 
 
 """
 
-# input_string_name = "./day_8/day_8_input.txt"
-input_string_name = "./day_8/day_8_input_test.txt"
+input_string_name = "./day_8/day_8_input.txt"
+# input_string_name = "./day_8/day_8_input_test.txt"
 
 
 # First read the contents of the input file into a list.
@@ -38,9 +36,63 @@ mat = [list(row) for row in input_list]
 
 
 
+# (1) For each frequency, identify all instances.
+
+dict_of_frequencies = {}
+
+for r in range(len(mat)):
+    for c in range(len(mat[r])):
+        if mat[r][c] != ".":
+            dict_of_frequencies.setdefault(mat[r][c], []).append([r,c])
 
 
+# (2) For each frequency, identify all pairs of instances.
+
+pairs = []
+
+for frequency in dict_of_frequencies:
+    for i in range(len(dict_of_frequencies[frequency])):
+        for j in range(i+1, len(dict_of_frequencies[frequency])):
+            pairs.append([dict_of_frequencies[frequency][i],dict_of_frequencies[frequency][j]])
+
+
+# (3) For each pair, identify the indexes of the two antinodes.
+
+diffs = []
+
+
+for pair in pairs:
+    diffs.append([pair[0][0] - pair[1][0], pair[0][1] - pair[1][1]])
+
+
+
+antinodes = []
+
+for k in range(len(diffs)):
+    antinodes.append([pairs[k][0][0] + diffs[k][0], pairs[k][0][1] + diffs[k][1]])
+    antinodes.append([pairs[k][1][0] - diffs[k][0], pairs[k][1][1] - diffs[k][1]])
+
+
+
+
+# (4) Count the number of antinodes which do not lie out of bounds
+
+antinode_count = 0
+
+# Remove duplciates from our list of antinodes:
+antinodes_deduplicated = []
+for antinode in antinodes:
+    if antinode not in antinodes_deduplicated:
+        antinodes_deduplicated.append(antinode)
+
+
+
+for x in antinodes_deduplicated:
+    if x[0] in range(len(mat)) and x[1] in range(len(mat[1])):
+        antinode_count += 1
+
+
+
+print(antinode_count)
 
 pass
-
-
